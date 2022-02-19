1f02206b16c2d759ba93166efeff9d69095804da
Mamadou lamine Bèye <laminebeye007@gmail.com>
2022-02-19 16:15:26 +0000

    budget controller added

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,28 +5,28 @@
         <form method="POST" action="{{ route('register') }}">
             @csrf
 
-            <div class="form-group">
+            <div class="form-group has-feedback feedback-left"">
                 <x-jet-label for="name" value="{{ __('Name') }}" />
                 <x-jet-input id="name" class="form-control" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             </div>
 
-            <div class="form-group">
+            <div class="form-group has-feedback feedback-left"">
                 <x-jet-label for="email" value="{{ __('Email') }}" />
                 <x-jet-input id="email" class="form-control" type="email" name="email" :value="old('email')" required />
             </div>
 
-            <div class="form-group">
+            <div class="form-group has-feedback feedback-left"">
                 <x-jet-label for="password" value="{{ __('Password') }}" />
                 <x-jet-input id="password" class="form-control" type="password" name="password" required autocomplete="new-password" />
             </div>
 
-            <div class="form-group">
+            <div class="form-group has-feedback feedback-left"">
                 <x-jet-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
                 <x-jet-input id="password_confirmation" class="form-control" type="password" name="password_confirmation" required autocomplete="new-password" />
             </div>
 
             @if (Laravel\Jetstream\Jetstream::hasTermsAndPrivacyPolicyFeature())
-                <div class="form-group">
+                <div class="form-group has-feedback feedback-left"">
                     <x-jet-label for="terms">
                         <div class="flex items-center">
                             <x-jet-checkbox name="terms" id="terms"/>
